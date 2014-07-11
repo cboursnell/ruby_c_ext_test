b15d118cf9825676a41bee93c856b2ef98b7a419
Chris Boursnell <cboursnell@gmail.com>
2014-07-11 17:17:38 +0100

    add and get methods working as intended with one count array

--- ext/mytest/mytest.c
+++ ext/mytest/mytest.c
@@ -1,44 +1,82 @@
 #include "ruby.h"
 
-// Defining a space for information and references about the module to be stored internally
+// Defining a space for information and references about the module to be
+// stored internally
 VALUE MyTest = Qnil;
 
 // Prototype for the initialization method - Ruby calls this, not you
 void Init_mytest();
 
 // methods are prefixed by 'method_' here
-VALUE method_test1(VALUE self);
-VALUE method_add(VALUE, VALUE, VALUE);
+VALUE TestInit(VALUE, VALUE, VALUE);
+VALUE method_add(VALUE, VALUE);
 VALUE method_get(VALUE, VALUE);
 VALUE method_hashing(VALUE, VALUE, VALUE);
-int hash(char *, int);
-int set[10];
+
+int hash(char *, int, int);
+
+long set_len;
+int *set;
+int kmer_size;
 
 // The initialization method for this module
 void Init_mytest() {
-    MyTest = rb_define_module("MyTest");
-    rb_define_method(MyTest, "add", method_add, 2);
+    MyTest = rb_define_class("MyTest", rb_cObject);
+    rb_define_method(MyTest, "initialize", TestInit, 2);
+    rb_define_method(MyTest, "add", method_add, 1);
     rb_define_method(MyTest, "get", method_get, 1);
     rb_define_method(MyTest, "hashing", method_hashing, 2);
 }
 
-// This is the method we added to test out passing parameters
-VALUE method_add(VALUE self, VALUE first, VALUE second) {
-    int a = NUM2INT(first);
-    int b = NUM2INT(second);
-    set[0] = a+b;
-    return INT2NUM(set[0]);
+// called when creating a new class
+VALUE TestInit(VALUE self, VALUE ks, VALUE size)
+{
+    int i;
+    set_len = NUM2INT(size);
+    set = malloc(set_len * sizeof(int));
+    for (i = 0; i < set_len; i++) {
+        set[i] = 0;
+    }
+    kmer_size = NUM2INT(ks);
+    // buildIubComplement();
+    return Qnil;
+}
+
+// takes in a fastq read sequence eq 100 bases
+// kmerise the string, hash each kmer and add to set
+VALUE method_add(VALUE self, VALUE read) {
+    char * str;
+    int i,h,len;
+    h = 0;
+    len = RSTRING_LEN(read);
+    str = StringValueCStr(read);
+    for (i = 0; i < len-kmer_size+1; i++) {
+        h = hash(str, i, kmer_size);
+        set[h] += 1;
+    }
+    return INT2NUM(1);
 }
 
 // This is the method we added to test out passing parameters
-VALUE method_get(VALUE self, VALUE index) {
-    int i = NUM2INT(index);
-    return INT2NUM(set[i]);
+VALUE method_get(VALUE self, VALUE kmer) {
+    char * str;
+    int len,h;
+    len = RSTRING_LEN(kmer);
+    str = StringValueCStr(kmer);
+    // inPlaceReverse(str, kmer_size);
+    // printf("kmer = %s\n",str);
+    if (len <= kmer_size) {
+        h = hash(str, 0, kmer_size);
+        // printf("hash = %i \n", h);
+        return INT2NUM(set[h]);
+    } else {
+        return 0;
+    }
 }
 
-int hash(char * str, int len) {
-    int i,hash=0;
-    for(i = 0; i < len; i++) {
+int hash(char * str, int start, int len) {
+    int i,hash=0,hash2=0;
+    for(i = start; i < start+len; i++) {
         hash = hash << 2;
         if (str[i]=='A') {
             hash += 0;
@@ -50,7 +88,26 @@ int hash(char * str, int len) {
             hash += 3;
         }
     }
-    return hash;
+    // calculate reverse complement hashing function
+    for(i = start+len-1;i>=start; i--) {
+        hash2 = hash2 << 2;
+        if (str[i]=='A') {
+            hash2 += 3;
+        } else if (str[i]=='C') {
+            hash2 += 2;
+        } else if (str[i]=='G') {
+            hash2 += 1;
+        } else if (str[i]=='T') {
+            hash2 += 0;
+        }
+    }
+    if (hash2 > hash) {
+        hash2 = hash2 % set_len;
+        return hash2;
+    } else {
+        hash = hash % set_len;
+        return hash;
+    }
 }
 
 VALUE method_hashing(VALUE self, VALUE kmer, VALUE n) {
@@ -59,6 +116,6 @@ VALUE method_hashing(VALUE self, VALUE kmer, VALUE n) {
     int output;
     len = RSTRING_LEN(kmer);
     str = StringValueCStr(kmer);
-    output = hash(str, len);
+    output = hash(str, 0, len);
     return INT2NUM(output);
 }
