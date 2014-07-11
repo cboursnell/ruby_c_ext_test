cd4f0943075c0d759dbc150a61a859728d94e65e
Chris Boursnell <cboursnell@gmail.com>
2014-07-11 13:19:18 +0100

    indentation of C and subtract method

--- ext/mytest/mytest.c
+++ ext/mytest/mytest.c
@@ -6,26 +6,38 @@ VALUE MyTest = Qnil;
 // Prototype for the initialization method - Ruby calls this, not you
 void Init_mytest();
 
-// Prototype for our method 'test1' - methods are prefixed by 'method_' here
+// methods are prefixed by 'method_' here
 VALUE method_test1(VALUE self);
 VALUE method_add(VALUE, VALUE, VALUE);
+VALUE method_subtract(VALUE, VALUE, VALUE);
+
 
 // The initialization method for this module
 void Init_mytest() {
-MyTest = rb_define_module("MyTest");
-rb_define_method(MyTest, "test1", method_test1, 0);
-rb_define_method(MyTest, "add", method_add, 2);
+    int set[10];
+    MyTest = rb_define_module("MyTest");
+    rb_define_method(MyTest, "test1", method_test1, 0);
+    rb_define_method(MyTest, "add", method_add, 2);
+    rb_define_method(MyTest, "subtract", method_subtract, 2);
+
 }
 
 // Our 'test1' method.. it simply returns a value of '10' for now.
 VALUE method_test1(VALUE self) {
-int x = 10;
-return INT2NUM(x);
+    int x = 10;
+    return INT2NUM(x);
 }
 
 // This is the method we added to test out passing parameters
 VALUE method_add(VALUE self, VALUE first, VALUE second) {
-int a = NUM2INT(first);
-int b = NUM2INT(second);
-return INT2NUM(a + b);
+    int a = NUM2INT(first);
+    int b = NUM2INT(second);
+    return INT2NUM(a + b);
+}
+
+// This is the method we added to test out passing parameters
+VALUE method_subtract(VALUE self, VALUE first, VALUE second) {
+    int a = NUM2INT(first);
+    int b = NUM2INT(second);
+    return INT2NUM(a - b);
 }
