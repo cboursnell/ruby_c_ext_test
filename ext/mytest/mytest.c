a5fa9837e12f05876ffbddf7cfd6480cfe10a1ca
Chris Boursnell <cboursnell@gmail.com>
2014-07-11 17:54:19 +0100

    added multiple buckets

--- ext/mytest/mytest.c
+++ ext/mytest/mytest.c
@@ -8,37 +8,41 @@ VALUE MyTest = Qnil;
 void Init_mytest();
 
 // methods are prefixed by 'method_' here
-VALUE TestInit(VALUE, VALUE, VALUE);
+VALUE TestInit(VALUE, VALUE, VALUE, VALUE);
 VALUE method_add(VALUE, VALUE);
 VALUE method_get(VALUE, VALUE);
 VALUE method_hashing(VALUE, VALUE, VALUE);
 
-int hash(char *, int, int);
+int hash(char *, int, int, int);
 
 long set_len;
+int set_count;
 int *set;
 int kmer_size;
 
 // The initialization method for this module
 void Init_mytest() {
     MyTest = rb_define_class("MyTest", rb_cObject);
-    rb_define_method(MyTest, "initialize", TestInit, 2);
+    rb_define_method(MyTest, "initialize", TestInit, 3);
     rb_define_method(MyTest, "add", method_add, 1);
     rb_define_method(MyTest, "get", method_get, 1);
     rb_define_method(MyTest, "hashing", method_hashing, 2);
 }
 
 // called when creating a new class
-VALUE TestInit(VALUE self, VALUE ks, VALUE size)
+// ks = size of kmer to use in bases
+// size = length of array
+// buckets = number of buckets
+VALUE TestInit(VALUE self, VALUE ks, VALUE size, VALUE count)
 {
     int i;
     set_len = NUM2INT(size);
-    set = malloc(set_len * sizeof(int));
-    for (i = 0; i < set_len; i++) {
+    set_count = NUM2INT(count);
+    set = malloc(set_len * set_count * sizeof(int));
+    for (i = 0; i < set_len * set_count; i++) {
         set[i] = 0;
     }
     kmer_size = NUM2INT(ks);
-    // buildIubComplement();
     return Qnil;
 }
 
@@ -46,36 +50,49 @@ VALUE TestInit(VALUE self, VALUE ks, VALUE size)
 // kmerise the string, hash each kmer and add to set
 VALUE method_add(VALUE self, VALUE read) {
     char * str;
-    int i,h,len;
+    int b,i,h,len;
     h = 0;
     len = RSTRING_LEN(read);
     str = StringValueCStr(read);
     for (i = 0; i < len-kmer_size+1; i++) {
-        h = hash(str, i, kmer_size);
-        set[h] += 1;
+        for(b = 0; b < set_count; b++) {
+            h = hash(str, i, kmer_size, b);
+            set[b*set_len+h] += 1;
+        }
     }
-    return INT2NUM(1);
+    return INT2NUM(1); // return 1 to indicate true /shrug
 }
 
 // This is the method we added to test out passing parameters
 VALUE method_get(VALUE self, VALUE kmer) {
     char * str;
-    int len,h;
+    int b,v=-1,p,len,h;
     len = RSTRING_LEN(kmer);
     str = StringValueCStr(kmer);
     // inPlaceReverse(str, kmer_size);
     // printf("kmer = %s\n",str);
     if (len <= kmer_size) {
-        h = hash(str, 0, kmer_size);
-        // printf("hash = %i \n", h);
-        return INT2NUM(set[h]);
+        for (b = 0; b < set_count; b++) {
+            h = hash(str, 0, kmer_size, b);
+            if (v < 0) {
+                v = set[b*set_len+h];
+            } else {
+                p = set[b*set_len+h];
+                if (p < v) {
+                    v = p;
+                }
+            }
+            // printf("hash = %i \n", h);
+        }
+        return INT2NUM(v);
     } else {
         return 0;
     }
 }
 
-int hash(char * str, int start, int len) {
-    int i,hash=0,hash2=0;
+int hash(char * str, int start, int len, int n) {
+    int r,i,hash=0,hash2=0;
+    srand(n+1);
     for(i = start; i < start+len; i++) {
         hash = hash << 2;
         if (str[i]=='A') {
@@ -101,6 +118,9 @@ int hash(char * str, int start, int len) {
             hash2 += 0;
         }
     }
+    r = rand();
+    hash += r;
+    hash2 += r;
     if (hash2 > hash) {
         hash2 = hash2 % set_len;
         return hash2;
@@ -114,8 +134,10 @@ VALUE method_hashing(VALUE self, VALUE kmer, VALUE n) {
     char * str;
     int len;
     int output;
+    int b;
     len = RSTRING_LEN(kmer);
     str = StringValueCStr(kmer);
-    output = hash(str, 0, len);
+    b = NUM2INT(n);
+    output = hash(str, 0, len, b);
     return INT2NUM(output);
 }
