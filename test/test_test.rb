424f2fc40c9993e1f53bd03e4ac2b77df2d764ab
Chris Boursnell <cboursnell@gmail.com>
2014-07-11 18:37:53 +0100

    check that the hash function works with longer kmers

--- test/test_test.rb
+++ test/test_test.rb
@@ -22,10 +22,25 @@ class TestMyTest < Test::Unit::TestCase
       assert_equal 1, @test.get("GTCAGAG") # TCTGACG
     end
 
+    should 'get the count of a kmer before anything is added' do
+      assert_equal 0, @test.get("GTCAGAG") # TCTGACG
+    end
+
+    # should 'get the median count' do
+    #   assert_equal 1, @test.add("TACGTACGTACGTTACGTAC")
+    #   assert_equal 1, @test.median("AACGTACGTACGTAAACGTA")
+    # end
+
     should 'hash a kmer' do
       assert_equal 70, @test.hashing("AAAAAAC", 0)
       assert_equal 2, @test.hashing("ACGTGCA", 0)
       assert_equal 95, @test.hashing("TTTCCCA", 0)
+    end
+
+    should 'hash a kmer with large k' do
+      test = MyTest.new(21, 100, 4)
+      v = test.hashing("ACGTGCATCGATTCGATCGAT", 0)
+      puts "value returned from large kmer = #{v}"
     end
   end
 end
