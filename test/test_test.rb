fe1eb51804ea2df36b99f2913b1873d8d638443d
Chris Boursnell <cboursnell@gmail.com>
2014-07-11 13:53:05 +0100

    test to hash a kmer

--- test/test_test.rb
+++ test/test_test.rb
@@ -17,8 +17,12 @@ class TestMyTest < Test::Unit::TestCase
       assert_equal 10, add(3,7)
     end
 
-    should 'subtract two numbers together' do
-      assert_equal 4, subtract(7,3)
+    should 'get the answer' do
+      assert_equal 10, get(0)
+    end
+
+    should 'hash a kmer' do
+      assert_equal 1, hashing("AAAAC", 0)
     end
   end
 end
