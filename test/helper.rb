3b04883684009d8ab60456972cf366390a0e55ed
Chris Boursnell <cboursnell@gmail.com>
2014-07-11 17:19:02 +0100

    require the c library

--- test/helper.rb
+++ test/helper.rb
@@ -10,8 +10,8 @@ SimpleCov.start
 require 'test/unit'
 begin; require 'turn/autorun'; rescue LoadError; end
 require 'shoulda-context'
-require 'mytest'
-include MyTest
+require 'mytest.so'
+# include MyTest
 
 Turn.config.format = :pretty
 Turn.config.trace = 5
