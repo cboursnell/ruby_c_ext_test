957c5bd7a9b067f0e6259ec8ae77fc308557aa1b
Chris Boursnell <cboursnell@gmail.com>
2014-07-13 00:53:22 +0100

    new test for new c run method

--- test/test_test2.rb
+++ test/test_test2.rb
@@ -7,7 +7,8 @@ class TestMyTest2 < Test::Unit::TestCase
   context 'my_test_ruby' do
 
     setup do
-      @test = MyTest.new(7, 100, 4)
+      # kmer_size=7, bucket_size=1000, bucket_count=4, cutoff=20
+      @test = MyTest.new(7, 100, 4, 20)
     end
 
     teardown do
@@ -17,7 +18,7 @@ class TestMyTest2 < Test::Unit::TestCase
 
     should 'respond to ' do
       assert_respond_to(@test, :add_reads)
-      assert_respond_to(@test, :run)
+      # assert_respond_to(@test, :run) # deprecated
       assert_respond_to(@test, :kmer_size)
     end
 
@@ -33,15 +34,24 @@ class TestMyTest2 < Test::Unit::TestCase
       end
     end
 
-    should 'run on reads' do
-      @test.add_reads("test/head.left.fastq", "test/head.right.fastq")
-      @test.run("output", 3)
-      assert File.exist?("output.1.fastq")
-      assert File.exist?("output.2.fastq")
-    end
+    # deprecated
+    # should 'run on reads' do
+    #   @test.add_reads("test/head.left.fastq", "test/head.right.fastq")
+    #   @test.run_all("output", 3)
+    #   assert File.exist?("output.1.fastq")
+    #   assert File.exist?("output.2.fastq")
+    # end
 
     should 'get the kmer size' do
       assert_equal 7, @test.kmer_size
+    end
+
+    should 'run the c method' do
+      left = "test/head.left.fastq"
+      right = "test/head.right.fastq"
+      left = File.expand_path(left)
+      right = File.expand_path(right)
+      @test.run(left, right)
     end
   end
 end
